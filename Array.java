d2883a9b785f48d83625eaf419a129351cf13406
milinia <memetovaevelina360@gmail.com>
2019-11-16 13:30:50 +0300

    first task

---
+++ Array.java
@@ -0,0 +1,27 @@
+//отсортировать массив методом вставки
+public class Array{
+ 	public static void main(String[] args) {
+ 		Integer[] array = new Integer[] {2,9,5,13,67,8,45};
+ 		int x = 1;
+
+ 		for (int i = 0; i < array.length - 1; i++){
+
+ 			for (int j = 0; j < x; j++){
+
+ 				if (array[j] > array[x]){
+
+ 					array[j] = array[j] + array[x];
+ 					array[x] = array[j] - array[x];
+ 					array[j] = array[j] - array[x];
+ 				}
+ 			}
+ 			x++;
+ 		}
+
+ 		for (int i = 0; i < array.length; i++){
+ 			System.out.println(array[i]);
+ 		}
+
+ 	}
+ }
+
